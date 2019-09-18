e1fb41f070a7516636b59613b6dd072dd6deb447
OkkaMuhiba <okka.muhiba@gmail.com>
2019-09-18 14:59:53 +0700

    init project

---
+++ src/main/java/com/future/booklook/BooklookApplication.java
@@ -0,0 +1,13 @@
+package com.future.booklook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklookApplication.class, args);
+    }
+
+}
