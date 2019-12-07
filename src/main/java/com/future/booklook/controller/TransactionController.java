923042e6cc9a8b3d61b1e320c6cfa376adb951d6
OkkaMuhiba <okka.muhiba@gmail.com>
2019-12-07 13:06:24 +0700

    Created BucketController, TransactionController, WishlistController

---
+++ src/main/java/com/future/booklook/controller/TransactionController.java
@@ -0,0 +1,12 @@
+package com.future.booklook.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+
+}
---
+++ src/main/java/com/future/booklook/controller/WishlistController.java
@@ -0,0 +1,7 @@
+package com.future.booklook.controller;
+
+import io.swagger.annotations.Api;
+
+@Api
+public class WishlistController {
+}
